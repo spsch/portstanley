e8046df396f394cbc1575383e0a874c8f195710e
Jan Svehlak <jan.svehlak@k-business.com>
2023-04-18 13:22:14 +0200

    final version

--- js/main.js
+++ js/main.js
@@ -53,6 +53,18 @@
         }
     });
     
+    //Pop Up
+    //with this first line we're saying: "when the page loads (document is ready) run the following script"
+    $(document).ready(function () {
+        //select the POPUP FRAME and show it
+        $("#popup").hide().fadeIn(1000);
+
+        //close the POPUP if the button with id="close" is clicked
+        $("#close").on("click", function (e) {
+            e.preventDefault();
+            $("#popup").fadeOut(1000);
+        });
+    });
     
     // Back to top button
     $(window).scroll(function () {
