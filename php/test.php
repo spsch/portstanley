15883516007e04c82e413108b11b2eba59ce6092
Jan Svehlak <jan.svehlak@gmail.com>
2024-10-25 14:13:33 +0200

    zavreno 25.10.2024

--- php/test.php
+++ php/test.php
@@ -1,6 +1,6 @@
 <?php
 	//delete image denni_menu.jpg from /img/denni_menu/
-	$file_pointer = "../img/denni_nabidka/denni_menu.jpg";
+	$file_pointer = "/home/portstanley.cz/www/img/denni_nabidka/test_menu.pdf";
 	
 	if (!unlink($file_pointer)) {
 		echo ("$file_pointer can not be deleted due to error");
